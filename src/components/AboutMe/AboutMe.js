e913a2414088aef251c3c30369993ad0b88b198d
Meder <medi_medi@mail.ru>
2022-04-30 11:10:08 +0300

    feat: component AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeaderBlock from '../HeaderBlock/HeaderBlock';
 import { Link } from 'react-router-dom';
+import imageDiploma from '../../images/imageDiploma.jpg'
 
 import './AboutMe.css';
 
@@ -8,31 +9,47 @@ function AboutMe () {
   return (
     <section className='about-me'>
       <HeaderBlock>Студент</HeaderBlock>
-      <main className='AboutMe__main'>
-        <div className='promo__main-container'>
-          <div className='promo__main-description'>
-            <h1 className='promo__title'>
-              Учебный проект студента факультета Веб-разработки.
-            </h1>
-            <h4 className='promo__subtitle'>
-              Листайте ниже, чтобы узнать больше про этот проект и его создателя.
-            </h4>
+      <main className='about-me__main'>
+        <div className='about-me__main-container'>
+          <div className='about-me__main-description'>
+            <h2 className='about-me__title'>
+              Медер
+            </h2>
+            <h3 className='about-me__subtitle'>
+              Фронтенд-разработчик, 37 лет
+            </h3>
+            <p className='about-me__text'>
+              Я родился в Кыргызской ССР, живу в Москве, закончил Факультет кибернетики и информационных технологий ОшТУ,
+              по специальности, "Прикладная математика и информатика". Люблю заниматься программированием, чтением научных литератур,
+              волейболом (1-е место в спартакиаде 2020 по Истринскому округу, Московской обл.), еще мне нравиться шахматы, футбол и пинг-понг.
+              Прошел курс Яндекс.Практикума по веб-разработке. Ищу работу front-end разработчика.
+            </p>
           </div>
-          <Link>
-            <button
-              type='button'
-              className='promo__info-button opacity'
-            >
-              Узнать больше
-            </button>
-          </Link>
-        </div>
           <img
-            className='promo__main-logo opacity'
-            src=''
-            alt='Тектовая картинка, вид земли из космоса'
+            className='about-me__image'
+            src={imageDiploma}
+            alt='Фотография студента'
           />
-        </main>
+        </div>
+        <ul className='social__list'>
+          <li className='social__item'>
+            <a className='social__link'
+              href=''
+              target='_blank' rel=''
+            >
+              Facebook
+            </a>
+          </li>
+          <li className='social__item'>
+            <a className=''
+              href=''
+              target='_blank' rel=''
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </main>
     </section>
   );
 }

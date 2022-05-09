870cd3e23829bafbc77b639aa39ec78ac87a8581
Meder <medi_medi@mail.ru>
2022-05-09 13:50:01 +0300

    refactor: component MoviesCardList

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -27,7 +27,7 @@ function MoviesCard(props) {
           alt='test'
         />
         <div className={`movies-card__description-container ${props.customMoviesCardDescriptionContainer}`}>
-          <h3 className='movies-card__subtitle opacity'>Бег это свобода</h3>
+          <h3 className='movies-card__subtitle opacity'>Бег это свобода Бег это свобода Бег это свобода</h3>
           <div className={`movies-card__like-image opacity ${props.customMoviesCardLikeImage}`} />
         </div>
         <p className='movies-card__duration'>

f5708983a6b092ee9662bcddbe68153338e69035
Meder <medi_medi@mail.ru>
2022-05-16 13:47:36 +0300

    feat: сделать асинхронные GET- и POST-запросы к API

---
+++ src/utils/auth.js
@@ -0,0 +1,45 @@
+// export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'http://localhost:3001';
+// export const BASE_URL = 'https://https://api.asman.students.nomoredomains.work/movies';
+
+const request = ({url, method = 'POST', token, body}) => {
+  const config = {
+    method: method,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      ...!!token && {'Authorization': `Bearer ${token}`},
+    },
+    ...!!body && {body: JSON.stringify(body)},
+    // credentials: 'include',
+  }
+  return fetch(`${BASE_URL}${url}`, config)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(response.status);
+  })
+}
+
+export const register = (password, email) => {
+  return request({
+    url: '/signup',
+    body: {password, email},
+  })
+}
+
+export const authorize = (password, email) => {
+  return request({
+    url: '/signin',
+    body: {password, email},
+  })
+};
+
+export const getContent = (token) => {
+  return request({
+    url: '/users/me',
+    method: 'GET',
+    token,
+  })
+}
